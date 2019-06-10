6e96757be0d6bef8d6eceb18c9f126f8671b3ca3
Jon Kurinsky <jkurinsky@gmail.com>
2019-06-10 16:25:46 -0500

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,30 @@ import PageContentStyleWrapper from "../components/pageContentStyleWrapper"
 
 export default () => (
   <Layout>
-    <PageContentStyleWrapper>contact</PageContentStyleWrapper>
+    <PageContentStyleWrapper>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6" />
+        </div>
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="submit" value="Send Message" />
+        <input type="reset" value="Clear" />
+      </form>
+    </PageContentStyleWrapper>
   </Layout>
 )
