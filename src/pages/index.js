827da0415fdb7c0b3516c6cceebc4a225f154701
Jon Kurinsky <jkurinsky@gmail.com>
2019-06-11 11:20:13 -0500

    set up profile page image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,48 @@
 import React from "react"
 import Layout from "../components/layout"
 import PageContentStyleWrapper from "../components/pageContentStyleWrapper"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
 
-export default () => (
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const ProfileImage = styled.div`
+  flex: 1;
+  border: 2px solid #313131;
+  border-radius: 3px;
+  margin: 5px;
+`
+
+const ProfileDetails = styled.div`
+  flex: 2;
+  margin: 5px;
+`
+
+export default ({ data }) => (
   <Layout>
-    <PageContentStyleWrapper>homepage</PageContentStyleWrapper>
+    <PageContentStyleWrapper>
+      <ProfileContainer>
+        <ProfileImage>
+          <Img fluid={data.file.childImageSharp.fluid} />
+        </ProfileImage>
+        <ProfileDetails>profile details</ProfileDetails>
+      </ProfileContainer>
+    </PageContentStyleWrapper>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "profile.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
